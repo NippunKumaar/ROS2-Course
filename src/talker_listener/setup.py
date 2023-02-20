3bbb29d085ea45027322d497b854afb31d9cb9ea
A A Nippun Kumaar <nippun05@gmail.com>
2023-02-20 10:42:31 +0530

    added talker_listener package

---
+++ src/talker_listener/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'talker_listener'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nippun',
+    maintainer_email='nippun05@gmail.com',
+    description='A simple publisher subscriber example',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = talker_listener.publisher:main',
+            'listener = talker_listener.subscriber:main',
+        ],
+    },
+)
