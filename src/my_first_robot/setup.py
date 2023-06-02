d68cc24fa26d39644ec347f282badb555ca666b9
A A Nippun Kumaar <nippun05@gmail.com>
2023-06-02 23:29:04 +0530

    added my_first_robot package

---
+++ src/my_first_robot/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'my_first_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nippun',
+    maintainer_email='nippun05@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'spwan_tortoisebot = my_first_robot.tortoise_bot_spawn_client:main',
+        ],
+    },
+)
